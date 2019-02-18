e2f002af0eec1050f1d75b118c3eaa5da6fc0931
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2019-02-18 12:34:48 +0300

    Добавил перое упражнение

---
+++ 03/function.js
@@ -0,0 +1,44 @@
+let movieName = "Алита: Боевой ангел"
+let movieRating = 7.3
+let moviePrice = 600
+if (movieRating >= 7 && moviePrice < 500) {
+  console.log("На " + movieName + " стоит сходить в кино.")
+} else {
+  console.log(movieName + " лучше посмотреть дома.")
+}
+
+movieName = "Спасти Ленинград"
+movieRating = 5.5
+moviePrice = 300
+if (movieRating >= 7 && moviePrice < 500) {
+  console.log("На " + movieName + " стоит сходить в кино.")
+} else {
+  console.log(movieName + " лучше посмотреть дома.")
+}
+
+movieName = "Дом, который построил Джек"
+movieRating = 7.2
+moviePrice = 500
+if (movieRating >= 7 && moviePrice < 500) {
+  console.log("На " + movieName + " стоит сходить в кино.")
+} else {
+  console.log(movieName + " лучше посмотреть дома.")
+}
+
+movieName = "Стекло"
+movieRating = 6.5
+moviePrice = 400
+if (movieRating >= 7 && moviePrice < 500) {
+  console.log("На " + movieName + " стоит сходить в кино.")
+} else {
+  console.log(movieName + " лучше посмотреть дома.")
+}
+
+movieName = "Ван Гог. На пороге вечности"
+movieRating = 7.2
+moviePrice = 450
+if (movieRating >= 7 && moviePrice < 500) {
+  console.log("На " + movieName + " стоит сходить в кино.")
+} else {
+  console.log(movieName + " лучше посмотреть дома.")
+}
