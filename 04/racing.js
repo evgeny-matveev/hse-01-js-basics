ecba7934c209ff6e5583fc7800d8f54e86bde064
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2019-02-19 13:25:44 +0300

    WIP: добавил слайды и первую часть задания

--- 04/racing.js
+++ 04/racing.js
@@ -1,1 +1,8 @@
 let scores = [22, 33, 49, 87, 82, 38, 18, 49, 87, 49, 73, 18, 56, 62]
+
+let i = 0
+while (i < scores.length) {
+  let msg = `Лошадь №${i + 1} пришла с результатом ${scores[i]}`
+  console.log(msg)
+  i += 1
+}
